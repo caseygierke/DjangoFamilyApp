10404e22e17acb1c9eff24130f6c4c7afba3cdab
Casey <caseygierke@gmail.com>
2019-04-23 19:14:47 -0600

    Add post functionality to the front end.

---
+++ blog/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+from .models import Post
+
+class PostForm(forms.ModelForm):
+	class Meta:
+		# Select the model to generate
+		model = Post
+		# Pick which fields you want it to render, note 
+		# that author will generate a drop down box because 
+		# it uses the foreign key.  We will not use author
+		fields = ('title', 'text')
