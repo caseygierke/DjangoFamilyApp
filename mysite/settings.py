6c0540cd4bd2c635893af11054345afd5a87f833
Casey <caseygierke@gmail.com>
2019-09-07 20:31:20 -0600

    Switch to PostGreSQL database.

--- mysite/settings.py
+++ mysite/settings.py
@@ -84,6 +84,14 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    },
+    'postgresql': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'django_family',
+        'USER': 'postgres',
+        'PASSWORD': 'Phr3atic',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
